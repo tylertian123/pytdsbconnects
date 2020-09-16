4d7396b21d677da22188adcec33a488df73f8c8a
Tyler Tian <tylertian123@gmail.com>
2020-09-16 00:10:10 -0400

    Make TDSBConnects a context manager
    Make TDSBConnects an async context manager to make sure the aiohttp
    session is closed. Improve and add docstrings and type annotations.

--- tdsb.py
+++ tdsb.py
@@ -1,4 +1,5 @@
 import aiohttp
+import asyncio
 import datetime
 import time
 import typing
@@ -13,6 +14,9 @@ class TDSBConnects:
     def __init__(self, auto_refresh: bool=True, min_token_life: float=30.0):
         """
         Constructor.
+
+        Although the session is created when the object is constructed, an access token
+        is not obtained. login() must be called before any operations.
         
         :param auto_refresh: Whether to automatically refresh the token.
         :param min_token_life: When a token is valid for less than this number of seconds,
@@ -26,8 +30,28 @@ class TDSBConnects:
         self._session = aiohttp.ClientSession(raise_for_status=True, headers={
             "X-Client-App-Info": self.X_CLIENT_APP_INFO,
         })
+    
+    async def close(self) -> None:
+        """
+        Close the session.
+        """
+        await self._session.close()
+        await asyncio.sleep(0.250)
+    
+    async def __aenter__(self) -> "TDSBConnects":
+        return self
+    
+    async def __aexit__(self, exc, *exc_info):
+        return await self.close()
 
-    def _update_auth(self, data: typing.Dict[str, typing.Any]):
+    def _update_auth(self, data: typing.Dict[str, typing.Any]) -> None:
+        """
+        Updates the auth token, refresh token and expiry time.
+
+        For internal use only.
+
+        :param data: A dict containing the auth data.
+        """
         self._token = data["access_token"]
         self._refresh_token = data["refresh_token"]
         self._token_expiry = time.time() + data["expires_in"]
@@ -36,7 +60,13 @@ class TDSBConnects:
             "X-Client-App-Info": self.X_CLIENT_APP_INFO,
         })
     
-    async def login(self, username: str, password: str):
+    async def login(self, username: str, password: str) -> None:
+        """
+        Login to the system. This must be called before any operations.
+
+        :param username: The username (student ID).
+        :param password: The password.
+        """
         resp = await self._session.post(self.API_URL + "token", data={
             "username": username,
             "password": password,
@@ -44,7 +74,10 @@ class TDSBConnects:
         })
         self._update_auth(await resp.json())
     
-    async def refresh_token(self):
+    async def refresh_token(self) -> None:
+        """
+        Refresh the access token.
+        """
         if self._refresh_token is None:
             return
         resp = await self._session.post(self.API_URL + "token", data={
@@ -53,13 +86,23 @@ class TDSBConnects:
         })
         self._update_auth(await resp.json())
     
-    async def refresh_if_expired(self):
+    async def refresh_if_expired(self) -> None:
+        """
+        Refresh the access token if it has expired.
+        """
         if self._refresh_token is None:
             return
         if time.time() + self._min_token_life >= self._token_expiry:
             await self.refresh_token()
     
-    async def _get_endpoint(self, endpoint):
+    async def _get_endpoint(self, endpoint) -> typing.Dict[str, typing.Any]:
+        """
+        Make a GET request to an endpoint.
+
+        For internal use only.
+
+        :param endpoint: The endpoint.
+        """
         if self._token is None:
             return
         if self._auto_refresh:
@@ -69,7 +112,7 @@ class TDSBConnects:
         return data
     
     async def get_timetable(self, school_id: int, date: datetime.date):
-        url = f"/api/TimeTable/GetTimeTable/Student/{school_id}/{date.day:02d}{date.month:02d}{date.year}"
+        url = f"api/TimeTable/GetTimeTable/Student/{school_id}/{date.day:02d}{date.month:02d}{date.year}"
         return await self._get_endpoint(url)
     
     async def get_user_info(self):
