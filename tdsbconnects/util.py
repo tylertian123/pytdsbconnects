1bf497ea0d7bbab0c1f472570e8873d6c259c1c9
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-09-16 20:09:45 -0400

    Fix timezone

--- tdsbconnects/util.py
+++ tdsbconnects/util.py
@@ -1,6 +1,8 @@
 import datetime
 import pytz
 
+TORONTO = pytz.timezone("America/Toronto")
+
 def parse_datetime(timestamp: str) -> datetime.datetime:
     """
     Convert a datetime string from the TDSB API to a Python datetime.
@@ -10,6 +12,6 @@ def parse_datetime(timestamp: str) -> datetime.datetime:
     if timestamp.endswith("Z"):
         return datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").replace(tzinfo=datetime.timezone.utc)
     try:
-        return datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").replace(tzinfo=pytz.timezone("America/Toronto"))
+        return TORONTO.localize(datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S"))
     except ValueError:
         return datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S%z")
